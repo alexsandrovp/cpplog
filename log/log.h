8281e4b96171a0c0b61c08b26cccf5ad06033622
Alex Vargas <alexsandrovp@gmail.com>
2014-05-13 01:24:16 -0300

    thread safety
    use unique_lock for thread safety
    create macros that ensure that the lock is released

--- log/log.h
+++ log/log.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <mutex>
 #include <iostream>
 #include <iomanip>
 
@@ -13,72 +14,85 @@
 #include <sys/types.h>
 #endif
 
-class logger
-{
-    public:
+namespace logdetails {
+	class logger
+	{
+		public:
 
-		template<typename T> std::wostream& operator <<(const T& thing) { return std::wclog << thing; }
+			//this weird name is to avoid people calling this function directly
+			//people should use the macro instead
+			std::wostream &w5EAFA343EC974AA7AF201953459463DD(wchar_t type)
+			{
+				locker.lock();
+				std::wclog << std::setfill(L'0')
+					<< get_current_date_time().c_str() << L' ' << type << L" P"
+					<< std::setw(5) << get_current_process_id() << L" T"
+					<< std::setw(5) << get_current_thread_id() << L' ';
+				return std::wclog;
+			}
 
-		logger &debug() { return write(); }
-		logger &info() { return write(L'I'); }
-		logger &warning() { return write(L'W'); }
-		logger &error() { return write(L'E'); }
-		logger &fatal() { return write(L'F'); }
+			void release()
+			{
+				std::wclog << std::endl;
+				if (locker.owns_lock()) locker.unlock();
+			}
 
-		static logger &Instance()
-		{
-			static logger instance;
-			return instance;
-		}
+			static logger &Instance()
+			{
+				static logger instance;
+				return instance;
+			}
 
-    private:
-		logger(){};
-		logger(logger const&){};
-		logger& operator=(logger const&){};
+		private:
 
-		logger &write(wchar_t type = L'D')
-		{
-			this->Instance() << L"\n" << std::setfill(L'0')
-				<< get_current_date_time().c_str() << L' ' << type << L" P"
-				<< std::setw(5) << get_current_process_id() << L" T"
-				<< std::setw(5) << get_current_thread_id() << L' ';
-			return this->Instance();
-		}
+			std::mutex logmutex;
+			std::unique_lock<std::mutex> locker;
 
-#ifdef COMPILE_OS_WINDOWS
+			logger(logger const&){};
+			logger& operator=(logger const&){};
+			logger() : logmutex(), locker(logmutex, std::defer_lock) {};
 
-		static unsigned long get_current_process_id() { return GetCurrentProcessId(); }
-		static unsigned long get_current_thread_id() { return GetCurrentThreadId(); }
+	#ifdef COMPILE_OS_WINDOWS
 
-		static std::wstring get_current_date_time()
-		{
-			SYSTEMTIME st;
-			GetSystemTime(&st);
-			std::wostringstream ss;
-			ss << std::setfill(L'0')
-				<< std::setw(4) << st.wYear << L'-'
-				<< std::setw(2) << st.wMonth << L'-'
-				<< std::setw(2) << st.wDay << L' '
-				<< std::setw(2) << st.wHour << L':'
-				<< std::setw(2) << st.wMinute << L':'
-				<< std::setw(2) << st.wSecond << L'.'
-				<< std::setw(3) << st.wMilliseconds;
-			return ss.str();
-		}
+			static unsigned long get_current_process_id() { return GetCurrentProcessId(); }
+			static unsigned long get_current_thread_id() { return GetCurrentThreadId(); }
 
-#else if __GNUC__
+			static std::wstring get_current_date_time()
+			{
+				SYSTEMTIME st;
+				GetSystemTime(&st);
+				std::wostringstream ss;
+				ss << std::setfill(L'0')
+					<< std::setw(4) << st.wYear << L'-'
+					<< std::setw(2) << st.wMonth << L'-'
+					<< std::setw(2) << st.wDay << L' '
+					<< std::setw(2) << st.wHour << L':'
+					<< std::setw(2) << st.wMinute << L':'
+					<< std::setw(2) << st.wSecond << L'.'
+					<< std::setw(3) << st.wMilliseconds;
+				return ss.str();
+			}
 
-		static unsigned long get_current_process_id() { return getppid(); }
-		static unsigned long get_current_thread_id() { return (unsigned long)syscall(224); }
+	#else if __GNUC__
 
-		static wstring get_current_date_time()
-		{
-			wchar_t buf[80];
-			time_t now = time(0);
-			struct tm tstruct = *localtime(&now);
-			wcsftime(buf, sizeof(buf), L"%Y-%m-%d %X", &tstruct);
-			return buf;
-		}
+			static unsigned long get_current_process_id() { return getppid(); }
+			static unsigned long get_current_thread_id() { return (unsigned long)syscall(224); }
 
-#endif
-};
+			static wstring get_current_date_time()
+			{
+				wchar_t buf[80];
+				time_t now = time(0);
+				struct tm tstruct = *localtime(&now);
+				wcsftime(buf, sizeof(buf), L"%Y-%m-%d %X", &tstruct);
+				return buf;
+			}
+
+	#endif
+	};
+}
+
+#define LOGDEBUG(x)		logdetails::logger::Instance().w5EAFA343EC974AA7AF201953459463DD(L'D') << x; logdetails::logger::Instance().release();
+#define LOGINFO(x)		logdetails::logger::Instance().w5EAFA343EC974AA7AF201953459463DD(L'I') << x; logdetails::logger::Instance().release();
+#define LOGWARNING(x)	logdetails::logger::Instance().w5EAFA343EC974AA7AF201953459463DD(L'W') << x; logdetails::logger::Instance().release();
+#define LOGERROR(x)		logdetails::logger::Instance().w5EAFA343EC974AA7AF201953459463DD(L'E') << x; logdetails::logger::Instance().release();
+#define LOGFATAL(x)		logdetails::logger::Instance().w5EAFA343EC974AA7AF201953459463DD(L'F') << x; logdetails::logger::Instance().release();
--- msvs/main.cpp
+++ msvs/main.cpp
@@ -3,15 +3,38 @@
 
 #include "stdafx.h"
 
+#include <thread>
+
 #include <log.h>
 
 using namespace std;
 
-logger& LOG = logger::Instance();
+void func1(const wstring &s)
+{
+	Sleep(1011);
+	LOGWARNING("function 1: string: " << s << " length: " << s.length());
+}
+
+void func2(const wstring &s)
+{
+	Sleep(1000);
+	LOGFATAL("function 2: string: " << s << " length: " << s.length());
+}
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	LOG.info() << "oi";
+	LOGINFO("oi");
+	
+	thread t1(func1, L"aiaiaiai");
+	thread t2(func2, L"ioioio");
+
+	LOGDEBUG("before joining");
+
+	t1.join();
+	t2.join();
+
+	LOGINFO("the end");
+
 	return 0;
 }
 
